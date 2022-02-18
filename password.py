da012cdba301eb35a8f34d3b68b60d353e054e37
tracy <tracyjacobs775@gmail.com>
2022-02-18 17:04:12 +0300

    add class User

---
+++ password.py
@@ -0,0 +1,40 @@
+class User:
+#     """
+#     Create user class to add new user
+#     """
+#     user_list = []
+
+# def __init__(self, username, password):
+#     """
+#     method that allows us to define properties of a user
+#     """
+#     self.username = username
+#     self.password = password
+
+# def save_user(self): 
+#     """  
+#     method that saves a new user
+#     """
+#     User.user_list.append(self)
+
+#     # @classmethod
+# def show_user(tracy):
+#     return tracy.user_list
+
+# def delete_user(self):
+#     """   
+#     method that deletes a saved account
+#     """
+#     User.user_list.remove(self)
+
+# class Credentils():
+#    """
+#    Creates credential class so as to create new object
+#    """ 
+#    credentils_list = []
+# #    @classmethod
+       
+# def verify_user(tracy,username,password):   
+#     """
+#     method to confirm whether the user is in our user_list
+#     """
