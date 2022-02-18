b2e3442cb6fcd91b13b9e3a6e27e7bc890a2455d
tracy <tracyjacobs775@gmail.com>
2022-02-18 21:46:22 +0300

    added test file

---
+++ password_test.py
@@ -0,0 +1,13 @@
+import unittest
+from password import  User, Credentials
+
+class TestClass(unittest.TestCase):
+    """
+    a test class that defines test cases for the User class
+    """
+
+def setUp(self):
+    """
+    method that is executed before each individual tets methods run
+    """
+    
