8ad044f7c730f9ce9ed29be8956bb3dfdd242529
tracy <tracyjacobs775@gmail.com>
2022-02-20 18:09:44 +0300

    add delete function

--- password manager.py
+++ password manager.py
@@ -20,4 +20,37 @@ def display_user():
     """    
     return User.display_user()
 
-    
+def login_user(username,password):
+    """
+    function to check whether the user already exists and logs them in
+    """
+    check_user = Credentials.verify_user(username,password)
+    return check_user
+
+def create_new_credential(account,username,password):
+    """
+    function that creates new credential for a user
+    """    
+    new_credential = Credentials(account,username,password)
+    return new_credential
+
+def save_credential(credentials):
+    """
+    function to save credentials to the credential list
+    """    
+    credentials.save_details()
+
+def display_account_details():
+     """
+     function that returns all saved credentials
+     """
+     return Credentials.display_credentials()
+ 
+def delete_credentials(credentials):
+     """
+     function to delete credential from credentials list 
+     """
+     credentials.delete_credentials()
+
+     
+
